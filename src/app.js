eeb652a808cba72cc477e705aedcd1349c2939ba
unknown <Aprendiz@MEDAPRCSGFSP730.FormacionSena.RED>
2025-04-21 22:39:59 -0500

    Primer commit

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import rutasLibros from './routes/libros.route.js';
+
 const PORT = process.env.PORT ?? 3000;
 
 const app = express();
-app.use(express.json())
+
+// Middleware para manejar JSON (solo una vez es necesario)
 app.use(express.json());
 
+// Ruta para los libros
 app.use('/api/libros', rutasLibros);
-
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
